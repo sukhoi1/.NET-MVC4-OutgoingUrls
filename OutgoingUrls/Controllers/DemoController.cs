ebf5fe11061d40afd0da2c1e0f8677107d320e7b
Anton Lyhin <Anton_Lyhin@epam.com>
2016-11-30 19:12:43 +0300

    v0.001 - Outgoing Urls, initial commit;

---
+++ OutgoingUrls/Controllers/DemoController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace OutgoingUrls.Controllers
+{
+    public class DemoController : Controller
+    {
+        public ActionResult CustomVariable()
+        {
+            return View();
+        }
+    }
+}
---
+++ OutgoingUrls/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OutgoingUrls.MvcApplication" Language="C#" %>
