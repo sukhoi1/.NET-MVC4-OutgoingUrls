668f0a7061c83e185e062dd10223c4fcd10613f9
Anton Lygin <lygin@live.com>
2016-12-01 01:40:11 +0300

    v0.002 - demonstrating that {controller} default value causes 'unpredictable' ruote choise (algorithm is not clear, but does not match the books 'model');

---
+++ OutgoingUrls.Test/InitailOutgoingurlsTest.cs
@@ -0,0 +1,50 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace OutgoingUrls.Test
+{
+    [TestClass]
+    public class InitailOutgoingurlsTest
+    {
+        private HttpContextBase CreateHttpContext(string targetUrl = null,
+                                                  string httpMethod = "GET")
+        {
+            // create the mock request 
+            Mock<HttpRequestBase> mockRequest = new Mock<HttpRequestBase>();
+            mockRequest.Setup(m => m.AppRelativeCurrentExecutionFilePath)
+                .Returns(targetUrl);
+            mockRequest.Setup(m => m.HttpMethod).Returns(httpMethod);
+
+            // create the mock response
+            Mock<HttpResponseBase> mockResponse = new Mock<HttpResponseBase>();
+            mockResponse.Setup(m => m.ApplyAppPathModifier(
+                It.IsAny<string>())).Returns<string>(s => s);
+
+            // create the mock context, using the request and response
+            Mock<HttpContextBase> mockContext = new Mock<HttpContextBase>();
+            mockContext.Setup(m => m.Request).Returns(mockRequest.Object);
+            mockContext.Setup(m => m.Response).Returns(mockResponse.Object);
+
+            // return the mocked context
+            return mockContext.Object;
+        }
+
+        [TestMethod]
+        public void VeryNice_TestOutgoingUrls_DependsOnDefaultControllerValue()
+        {
+            // Arrange
+            RouteCollection routes = new RouteCollection();
+            RouteConfig.RegisterRoutes(routes);
+            RequestContext context = new RequestContext(CreateHttpContext(), new RouteData());
+
+            // Act
+            string result = UrlHelper.GenerateUrl(null, "Index", "Home", null, routes, context, true);
+
+            // Assert
+            Assert.AreEqual("/App/DoIndex", result);
+        }
+    }
+}
