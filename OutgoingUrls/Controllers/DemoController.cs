3f5023375912d7fc875ed8705beb443422d20237
Anton Lyhin <Anton_Lyhin@epam.com>
2016-12-05 19:57:30 +0300

    v0.006 - Html helper which reuses a partial view, example (do not generate html in a helper! Html generation violates separation of concerns principle);

--- OutgoingUrls/Controllers/DemoController.cs
+++ OutgoingUrls/Controllers/DemoController.cs
@@ -1,10 +1,16 @@
 ﻿using System.Web.Mvc;
+using System.Web.Mvc.Routing.Constraints;
 
 namespace OutgoingUrls.Controllers
 {
     public class DemoController : Controller
     {
         public ActionResult CustomVariable()
+        {
+            return View();
+        }
+
+        public ActionResult Alpha()
         {
             return View();
         }
---
+++ OutgoingUrls/Helper/AlphaHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace OutgoingUrls.Helper
+{
+    public static class AlphaHelperClass
+    {
+        public static MvcHtmlString AlphaHelper<T>(this HtmlHelper helper, string absolutePathToView, T model)
+        {
+            return helper.Partial(absolutePathToView, model);
+        }
+    }
+}
